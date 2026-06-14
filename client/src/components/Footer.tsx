2df6f11625d3891ab6efa821246870b15a6ea573
Raghav khandelwal <raghavkhandelwal483@gmail.com>
2026-06-14 23:09:58 +0530

    added responsiveness

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
             TRENDLAMA.
           </p>
         </Link>
-        <p className="text-sm text-gray-400">© 2025 Trendlama.</p>
+        <p className="text-sm text-gray-400">© 2026 Trendlama.</p>
         <p className="text-sm text-gray-400">All rights reserved.</p>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
